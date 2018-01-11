2fccd61c218a704513b247a6dc56943bcb61622c
Rodrigo Vivi <rodrigo.vivi@intel.com>
2018-01-11 16:00:03 -0200

    drm/i915/cnl: Add Port F definition.
    BugLink: http://bugs.launchpad.net/bugs/1757573
    
    Some Cannonlake SKUs will come with a full split between
    port A and port E. This will be called port F although it
    is not a 6th port, but only a split.
    
    Note this patch alone is not sufficient for port F enabling,
    it's just the first step.
    
    v2: Fix size of dvo_ports found by Ander.
    v3: Adding missing cases from intel_bios.c for Port_F
    v4: Adding other missing cases and fix the commit message.
    v5: Rebase on top of display headers rework.
    v6 (from Paulo): improve commit message, bikeshed bit definitions.
    
    Cc: Lucas De Marchi <lucas.demarchi@intel.com>
    Cc: Manasi Navare <manasi.d.navare@intel.com>
    Acked-by: Rodrigo Vivi <rodrigo.vivi@intel.com>
    Reviewed-by: Paulo Zanoni <paulo.r.zanoni@intel.com>
    Signed-off-by: Rodrigo Vivi <rodrigo.vivi@intel.com>
    Signed-off-by: Paulo Zanoni <paulo.r.zanoni@intel.com>
    Link: https://patchwork.freedesktop.org/patch/msgid/20180111180010.24357-2-paulo.r.zanoni@intel.com
    (backported from git://anongit.freedesktop.org/drm/drm-intel commit 841b5ed7aaec4578cd7303fb7f456f01b90dd313)
    Signed-off-by: Timo Aaltonen <timo.aaltonen@canonical.com>

--- drivers/gpu/drm/i915/intel_dp.c
+++ drivers/gpu/drm/i915/intel_dp.c
@@ -1392,6 +1392,7 @@ static i915_reg_t skl_aux_ctl_reg(struct drm_i915_private *dev_priv,
 	case PORT_B:
 	case PORT_C:
 	case PORT_D:
+	case PORT_F:
 		return DP_AUX_CH_CTL(port);
 	default:
 		MISSING_CASE(port);
@@ -1407,6 +1408,7 @@ static i915_reg_t skl_aux_data_reg(struct drm_i915_private *dev_priv,
 	case PORT_B:
 	case PORT_C:
 	case PORT_D:
+	case PORT_F:
 		return DP_AUX_CH_DATA(port, index);
 	default:
 		MISSING_CASE(port);
